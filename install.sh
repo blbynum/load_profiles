741e7243b9861403440bddbda2341c31cf42ca54
Ben Bynum <ben@bbynum.com>
2022-02-24 20:42:29 -0500

    Delete .bash_profile entries on install if already exist

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-LOADPROFS_HOME="/Library/Loadprofs"
+LOADPROFS_HOME='/Library/Loadprofs'
 bash_profile="$HOME/.bash_profile"
 
 echo "Installing Loadprofs..."
@@ -13,12 +13,15 @@ fi
 
 rsync -av --exclude='install.sh' --exclude='res' --exclude='.git' --exclude='.git*' ./ $LOADPROFS_HOME/ 
 
-first="export LOADPROFS_HOME="$LOADPROFS_HOME""
+first="export LOADPROFS_HOME=\"$LOADPROFS_HOME\""
 second=". $LOADPROFS_HOME/bin/.loadprofs_profile"
 
-echo -e $first >> $bash_profile
-echo -e $second >> $bash_profile
-echo -e "" >> $bash_profile
+echo "$(grep -v "$first" $bash_profile)" > $bash_profile
+echo "$(grep -v "$second" $bash_profile)" > $bash_profile
 
-echo "Loadprofs installed sucessfully! Restart terminal or run source \`~/.bash_profile\`"
+echo -e $first  >> $bash_profile
+echo -e $second >> $bash_profile
+echo -e ""      >> $bash_profile
+
+echo "Loadprofs installed sucessfully! Restart terminal or run \"source ~/.bash_profile\""
 
